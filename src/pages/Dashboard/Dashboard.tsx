134288ad08b7554fa4c47d064646acf5e9d50899
Vinay Maheshwari <vinaymaheshwari35@gmail.com>
2022-12-13 15:41:27 +0530

    dashboard-fix

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -3,20 +3,36 @@ import { connect } from 'react-redux/es/exports';
 import QuestionWithIframe from '../../components/QuestionWIthIframe';
 
 const Dashboard = ({ dashboard, district, block, cluster }: any) => {
-
-
+    const _dash = {
+        performance: 26,
+        admin: 25,
+        academic: 27
+    }
+    const compliment = ((dashboard === _dash.performance) || (dashboard === _dash.admin) || (dashboard === _dash.academic))
+    console.log(compliment, "this is")
     return (
         <div className='Dashboard'>
             <div className='Dashboard__iframeContainer'>
-                <QuestionWithIframe
-                    questionId={dashboard}
-                    type={1}
-                    width={"100%"}
-                    params={district ? { district } : block ? { block } : { cluster }}
-                    height="100%"
-                    nonDownloadable={true}
-                    handleLoadCounter={() => { }}
-                />
+                {compliment ? (
+                    <QuestionWithIframe
+                        questionId={dashboard}
+                        type={1}
+                        width={"100%"}
+                        height="100%"
+                        nonDownloadable={true}
+                        handleLoadCounter={() => { }}
+                    />
+                ) : (
+                    <QuestionWithIframe
+                        questionId={dashboard}
+                        type={1}
+                        width={"100%"}
+                        params={district ? { district } : block ? { block } : { cluster }}
+                        height="100%"
+                        nonDownloadable={true}
+                        handleLoadCounter={() => { }}
+                    />
+                )}
             </div>
         </div >
     )
