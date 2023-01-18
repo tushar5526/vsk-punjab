1cf45fda0e48419832504f9f201e26337082c093
Vinay Maheshwari <vinaymaheshwari35@gmail.com>
2023-01-18 13:11:28 +0530

    made changes

--- src/components/MapComponent/ToolTip/ToolTip.js
+++ src/components/MapComponent/ToolTip/ToolTip.js
@@ -36,10 +36,9 @@ const ToolTip = (props) => {
   return (
     <Marker position={props.position} icon={props.iconPerson}>
       <Popup
-        onOpen={() => {
-          console.log("hello");
-          getToolTipData();
-        }}
+        // onOpen={() => {
+        //   getToolTipData();
+        // }}
         className="tooltip-popup"
       >
         {toolTip.loaded ? (
